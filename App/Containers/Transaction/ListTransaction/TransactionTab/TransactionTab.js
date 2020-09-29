ddaae136b3324644f460d1c0ea9672247a297c51
nguyensonghao94 <hao.nguyensong@powergatesoftware.com>
2020-09-29 14:48:22 +0700

    add pull to reresh to list transaction

--- App/Containers/Transaction/ListTransaction/TransactionTab/TransactionTab.js
+++ App/Containers/Transaction/ListTransaction/TransactionTab/TransactionTab.js
@@ -2,47 +2,38 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  FlatList
+  FlatList,
+  RefreshControl
 } from 'react-native';
 import PropTypes from 'prop-types';
 
+import { commom } from '~/Themes';
 import styles from './TransactionTabStyle';
-import { commom } from '../../../../Themes';
-import TransactionItem from '../../../../Components/TransactionItem/TransactionItem';
+import TransactionItem from '~/Components/TransactionItem/TransactionItem';
 
 class TransactionTab extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      refreshing: false,
       listTransaction: []
     }
   }
 
   componentDidMount() {
+    const mocks = [];
+    for (let i = 0; i < 20; i++) {
+      mocks.push({
+        id: i.toString(),
+        title: `Item ${i}`,
+        detail: 'thanh toan abc',
+        createdAt: '20/10/2020',
+        money: '2500000'
+      })
+    }
+
     this.setState({
-      listTransaction: [
-        {
-          id: '1',
-          title: 'First Item',
-          detail: 'thanh toan abc',
-          createdAt: '20/10/2020',
-          money: '2500000'
-        },
-        {
-          id: '2',
-          title: 'First Item',
-          detail: 'thanh toan abc',
-          createdAt: '20/10/2020',
-          money: '2500000'
-        },
-        {
-          id: '3',
-          title: 'First Item',
-          detail: 'thanh toan abc',
-          createdAt: '20/10/2020',
-          money: '2500000'
-        },
-      ]
+      listTransaction: mocks
     })
   }
 
@@ -50,8 +41,12 @@ class TransactionTab extends Component {
     this.props.navigation.navigate('LOGIN_SCREEN');
   }
 
+  refreshListTransaction() {
+    console.log('Loading!');
+  }
+
   render() {
-    const { listTransaction } = this.state;    
+    const { listTransaction, refreshing } = this.state;    
 
     const renderItem = ({item}) => (
       <TransactionItem transaction={item} view={() => this.viewTransaction(item)} />
@@ -64,6 +59,12 @@ class TransactionTab extends Component {
           data={listTransaction}
           renderItem={renderItem}
           keyExtractor={item => item.id}
+          refreshControl={
+            <RefreshControl
+             refreshing={refreshing}
+             onRefresh={() => this.refreshListTransaction()}
+            />
+          }
         />
       </View>
     )
